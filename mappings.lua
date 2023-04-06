acb5c2dad44692ccd71d3297cbccce57f08dbba7
Ryosuke Hata <tech@r-hata.net>
2023-04-06 16:57:45 +0000

    Add aerial.nvim key mappings

--- mappings.lua
+++ mappings.lua
@@ -1,17 +1,33 @@
 local luasnip = require("luasnip")
+local aerial = require("aerial")
 
 return {
   n = {
     -- For US keyboard
-    [";"]     = { ":", noremap = true },
-    [":"]     = { ";", noremap = true },
+    [";"]         = { ":", noremap = true },
+    [":"]         = { ";", noremap = true },
     -- yankround
-    ["p"]     = { "<Plug>(yankround-p)" },
-    ["P"]     = { "<Plug>(yankround-P)" },
-    ["gp"]    = { "<Plug>(yankround-gp)" },
-    ["gP"]    = { "<Plug>(yankround-gP)" },
-    ["<C-p>"] = { "<Plug>(yankround-prev)" },
-    ["<C-n>"] = { "<Plug>(yankround-next)" },
+    ["p"]         = { "<Plug>(yankround-p)" },
+    ["P"]         = { "<Plug>(yankround-P)" },
+    ["gp"]        = { "<Plug>(yankround-gp)" },
+    ["gP"]        = { "<Plug>(yankround-gP)" },
+    ["<C-p>"]     = { "<Plug>(yankround-prev)" },
+    ["<C-n>"]     = { "<Plug>(yankround-next)" },
+    ["<leader>a"] = {
+      function()
+        aerial.toggle({
+          focus = true,
+          direction = "right",
+        })
+      end,
+      desc = "Toggle Aerial",
+    },
+    ["<leader>i"] = {
+      function()
+        aerial.focus()
+      end,
+      desc = "Focus Aerial",
+    }
   },
   x = {
     -- yankround
