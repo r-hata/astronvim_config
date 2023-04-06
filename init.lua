aa39560b81172e817758a87fe46b6ec167f57307
Ryosuke Hata <tech@r-hata.net>
2023-04-06 13:33:58 +0000

    Add automatic goimports

--- init.lua
+++ init.lua
@@ -1,3 +1,10 @@
 return {
   colorscheme = "tokyonight",
+  -- auto import for golang
+  vim.api.nvim_create_autocmd('BufWritePre', {
+    pattern = '*.go',
+    callback = function()
+      vim.lsp.buf.code_action({ context = { only = { 'source.organizeImports' } }, apply = true })
+    end
+  })
 }
