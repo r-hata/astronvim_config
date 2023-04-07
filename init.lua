9f4debe2ba664d3350a05cc77b0ba023f803f231
Ryosuke Hata <tech@r-hata.net>
2023-04-07 01:38:10 +0000

    Add indents

--- init.lua
+++ init.lua
@@ -1,10 +1,14 @@
 return {
+  -- colorscheme
   colorscheme = "tokyonight",
-  -- auto import for golang
-  vim.api.nvim_create_autocmd('BufWritePre', {
-    pattern = '*.go',
-    callback = function()
-      vim.lsp.buf.code_action({ context = { only = { 'source.organizeImports' } }, apply = true })
-    end
-  })
+  -- golang
+  {
+    -- auto import for golang
+    vim.api.nvim_create_autocmd('BufWritePre', {
+      pattern = '*.go',
+      callback = function()
+        vim.lsp.buf.code_action({ context = { only = { 'source.organizeImports' } }, apply = true })
+      end
+    }),
+  },
 }
