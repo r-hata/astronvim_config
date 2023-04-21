d7f2bce1fe4d614c68aa627ab29cf654ff965793
Ryosuke Hata <tech@r-hata.net>
2023-04-21 11:11:09 +0000

    Add VDsplit command

--- init.lua
+++ init.lua
@@ -1,4 +1,8 @@
 return {
   -- colorscheme
   colorscheme = "tokyonight",
+  { -- user commands
+    -- :VDsplit
+    vim.api.nvim_create_user_command("VDsplit", "vertical diffsplit <args>", { nargs = 1, complete = "file" }),
+  }
 }
