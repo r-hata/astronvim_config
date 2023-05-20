a542d425b5d72c451d840a476f0c82b67769c6fe
Ryosuke Hata <tech@r-hata.net>
2023-05-20 17:44:36 +0000

    Add astrocommunity pack.markdown

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,6 +4,7 @@ return {
   { import = "astrocommunity.pack.go" },
   { import = "astrocommunity.pack.json" },
   { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.markdown" },
   { import = "astrocommunity.pack.python" },
   { import = "astrocommunity.pack.rust" },
   { import = "astrocommunity.pack.yaml" },
--- plugins/nvim-treesitter.lua
+++ plugins/nvim-treesitter.lua
@@ -1,14 +1,4 @@
-local utils = require "astronvim.utils"
 return {
   "nvim-treesitter/nvim-treesitter",
-  opts = function(_, opts)
-    -- Ensure that opts.ensure_installed exists and is a table or string "all".
-    if not opts.ensure_installed then
-      opts.ensure_installed = {}
-    elseif opts.ensure_installed == "all" then
-      return
-    end
-    utils.list_insert_unique(opts.ensure_installed, { "cpp", "markdown" })
-    opts.ignore_install = { "bash" }
-  end,
+  opts = { ignore_install = { "bash" } },
 }
