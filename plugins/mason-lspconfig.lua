542f4edac01b4f8b83a4b66921bd6cfb6851edfa
Ryosuke Hata <tech@r-hata.net>
2023-05-20 17:47:04 +0000

    Delete vimls from ensure_installed

--- plugins/mason-lspconfig.lua
+++
@@ -1,9 +0,0 @@
-local utils = require "astronvim.utils"
-return {
-  "williamboman/mason-lspconfig.nvim",
-  opts = function(_, opts)
-    -- Ensure that opts.ensure_installed exists and is a table.
-    if not opts.ensure_installed then opts.ensure_installed = {} end
-    utils.list_insert_unique(opts.ensure_installed, "vimls")
-  end,
-}
